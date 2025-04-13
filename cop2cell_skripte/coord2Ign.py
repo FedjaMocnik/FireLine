9ffac7112fdbb23316bbffe9f796a2673a560fa4
mmalensek <marto3304@gmail.com>
2025-04-13 14:14:28 +0200

    Popravljen coord2Ign.py verzija 3

--- cop2cell_skripte/coord2Ign.py
+++ cop2cell_skripte/coord2Ign.py
@@ -1,11 +1,16 @@
 # dobim tri koordinate in ncols in nrows in vrnem
 import csv
 
-def coord2Ign(xllcorner: int, yllcorner: int, tockaX: int, tockaY: int,  nrow: int, ncol: int, cellsize: int):
-    col = int((tockaX - xllcorner) / cellsize)
+def coord2Ign(maxX: int, minY: int, minX: int, maxY: int, tockaX: int, tockaY: int,  nrow: int, ncol: int):
+    lat_ratio = (maxX - tockaX) / (maxX - minX)
+    lon_ratio = (maxY - tockaY) / (maxY - minY)
+    row = int(lat_ratio * nrow)
+    col = int(lon_ratio * ncol)
 
-    row = int((tockaY - yllcorner) / cellsize)
-    row = nrow - 1 - row
+    row = min(max(row, 0), nrow - 1)
+    col = min(max(col, 0), ncol - 1)
+    return row * ncol + col
+
 
     with open("cop2cell_skripte/results/Ignitions.csv", "w", newline="") as f:
         writer = csv.writer(f)
@@ -13,7 +18,7 @@ def coord2Ign(xllcorner: int, yllcorner: int, tockaX: int, tockaY: int,  nrow: i
         writer.writerow([1, row * ncol + col])
 
 def main():
-    print(coord2Ign(1, 1, 1, 1, 1, 1, 1))
+    print(coord2Ign(460000, 140000, 450000, 150000, 455000, 145000, 100, 100))
 
 
 if __name__ == "__main__":
